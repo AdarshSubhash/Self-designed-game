f6102ea55d9c47278410e60234845c07288cf313
AdarshSubhash <61652110+AdarshSubhash@users.noreply.github.com>
2020-05-23 17:51:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var Car;
+var ground, invisibleGround, groundImage;
+var Cari;
+
+function preload(){
+  Cari=loadImage("Images/Car.png");
+
+}
+
+function setup() {
+  createCanvas(600, 200);
+  Car=createSprite(150,80,30,20);
+  Car.addImage(Cari);
+}
+
+function draw() {
+  background(255);
+ 
+
+
+
+
+
+
+
+drawSprites;
+    }
+ 
